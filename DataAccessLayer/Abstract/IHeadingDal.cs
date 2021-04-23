642bba74719b06bb0d895df2d688411e25e0a478
ipekg <ipekgzel8@gmail.com>
2021-04-24 00:21:35 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IHeadingDal.cs
@@ -0,0 +1,12 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IHeadingDal:IRepository<Heading>
+    {
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    class CategoryRepository : ICategoryDal
+    {
+        //tolist , add, remove, find
+        Context Context = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category category)
+        {
+            _object.Remove(category);
+            Context.SaveChanges();
+        }
+
+        public void Insert(Category category)
+        {
+            _object.Add(category);
+            Context.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Task, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            Context.SaveChanges();
+        }
+    }
+}
